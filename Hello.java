0df30fbb07e8a8c2218e997cfd135e81d9d446e9
tumansutradhar <officialbishalsutradhar@gmail.com>
2024-12-02 19:06:13 +0530

    add java and class files

---
+++ Hello.java
@@ -0,0 +1,39 @@
+class Hello {
+    public static void main(String a[]) {
+
+        // 1. First Code in Java
+        // System.out.println("Hello World");
+
+        // 2. Variable in Java
+        // int num1 = 3;
+        // int num2 = 3;
+        // int result = num1 + num2;
+        // System.out.println(result);
+
+        // 3. Data Types In Java
+        // int i = 3;
+        // byte b = 127;
+        // short s = 555;
+        // long l = 5665l;
+
+        // float f = 5.5f;
+        // double d = 5.8;
+
+        // char c = 'a';
+
+        // boolean bl = true;
+
+        // 4. Literal in Java
+        // int num = 0b101; // Binary Convert
+        // int num = 0x8E; // Hexadecimal Convert
+        // int num = 10_00_11;
+        // double num = 100; // integer Convert
+        // double num = 100e40; // Scientific Notation
+        // System.out.println(num);
+        // char c = 'a';
+        // c++; // Increment Character
+        // System.out.println(c);
+
+        // 5. Type Conversion in Java
+    }
+}
