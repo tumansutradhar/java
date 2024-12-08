02270a8dbee9dddd06128fc18735d442bebdb6f5
tumansutradhar <officialbishalsutradhar@gmail.com>
2024-12-08 14:30:41 +0530

    add operators

---
+++ Demo.java
@@ -0,0 +1,61 @@
+public class Demo {
+    public static void main(String[] args) {
+        // 7. Relational Operators In Java
+        // int x = 8;
+        // int y = 4;
+
+        // boolean result = x < y; // Less Than
+        // boolean result = x > y; // Greater Than
+        // boolean result = x != y; // Not Equal
+        // boolean result = x == y; // Double Equal
+
+        // System.out.println(result);
+
+        // int x = 8;
+        // int y = 8;
+
+        // boolean result = x > y; // false
+        // boolean result = x >= y; // true
+
+        // System.out.println(result);
+
+        // double x = 9.99;
+        // double y = 10.4;
+
+        // boolean result = x <= y; // Less Than Equal
+        // boolean result = x >= y; // Greater Than Equal
+
+        // System.out.println(result);
+
+        // 8. Logical Operators In Java
+        // a) AND Operator(&&):
+        // true && true -> true
+        // true && false -> false
+        // false && true -> false
+        // false && false -> false
+
+        // b) OR Operator(||):
+        // true || true -> true
+        // true || false -> true
+        // false || true -> true
+        // false || false -> false
+
+        // c) NOT Operator(!):
+        // true -> false
+        // false -> true
+
+        // int x = 8;
+        // int y = 10;
+        // int a = 8;
+        // int b = 5;
+
+        // boolean result = x < y && a > b;
+        // boolean result = x > y || a > b;
+
+        // System.out.println(result);
+
+        // boolean result = x > y;
+
+        // System.out.println(!result);
+    }
+}
