573759ca1c24b853925855b1778c08413c479fa4
tumansutradhar <officialbishalsutradhar@gmail.com>
2024-12-09 23:16:39 +0530

    add loops

---
+++ Demo3.java
@@ -0,0 +1,41 @@
+public class Demo3 {
+    public static void main(String[] args) {
+        // 12. For Loop In Java
+        // a) While Loop
+        // int i = 1;
+
+        // while (i <= 4) {
+        // System.out.println(i + ". Hello World!");
+        // i++;
+        // }
+
+        // Nested Loop
+        // while (i <= 5) {
+        // System.out.println(i + ". Hello World!");
+
+        // int j = 1;
+
+        // while (j <= 3) {
+        // System.out.println("Have a nice day: " + j);
+        // j++;
+        // }
+
+        // i++;
+        // }
+
+        // System.out.println(i + ". </>");
+
+        // b) Do While Loop In Java
+        // int i = 7;
+
+        // do {
+        // System.out.println(i + ". Hello World!");
+        // i++;
+        // } while (i <= 5);
+
+        // c) For Loop In Java
+        // for (int i = 1; i <= 10; i++) {
+        // System.out.println(i + ". Hello World!");
+        // }
+    }
+}
