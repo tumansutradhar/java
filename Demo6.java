166faed9959fc4c53989c1de5492b230ef184f56
tumansutradhar <officialbishalsutradhar@gmail.com>
2024-12-12 00:48:00 +0530

    add string

---
+++ Demo6.java
@@ -0,0 +1,71 @@
+// class Student {
+//     String name;
+//     int rollno;
+//     int marks;
+// }
+
+public class Demo6 {
+    public static void main(String[] args) {
+        // 20. Array Of Objects In Java
+        // int newArray[] = new int[4];
+
+        // newArray[0] = 5;
+        // newArray[1] = 74;
+        // newArray[2] = 65;
+        // newArray[3] = 85;
+
+        // for (int i = 0; i < newArray.length; i++) {
+        // System.out.println(newArray[i]);
+        // }
+
+        // Student a = new Student();
+        // a.name = "Tuman";
+        // a.rollno = 20223045;
+        // a.marks = 787;
+
+        // Student b = new Student();
+        // b.name = "Bishal";
+        // b.rollno = 45;
+        // b.marks = 78;
+
+        // Student c = new Student();
+        // c.name = "Bunny";
+        // c.rollno = 01;
+        // c.marks = 98;
+
+        // System.out.println(a);
+        // System.out.println(b);
+        // System.out.println(c);
+
+        // Student students[] = new Student[3];
+        // students[0] = a;
+        // students[1] = b;
+        // students[2] = c;
+
+        // for (int i = 0; i < students.length; i++) {
+        // System.out.println(students[i].name + ": " + students[i].rollno);
+        // }
+
+        // 21. Enhanced For Loop In Java
+        // int a[] = new int[4];
+
+        // a[0] = 56;
+        // a[1] = 89;
+        // a[2] = 97;
+        // a[3] = 79;
+
+        // for (int i : a) {
+        // System.out.print(i + " ");
+        // }
+        // System.out.println();
+
+        // 22. String In Java
+        // String str = new String("Tuman");
+
+        // System.out.println(str);
+        // System.out.println(str.charAt(3));
+        // System.out.println(str.hashCode());
+        // System.out.println(str.concat(" Sutradhar"));
+
+    }
+}
