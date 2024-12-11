0b1cd1011e26a088be0dbed63bca16753236fee0
tumansutradhar <officialbishalsutradhar@gmail.com>
2024-12-11 10:06:12 +0530

    add overloading and class

---
+++ Demo4.java
@@ -0,0 +1,64 @@
+// class Math {
+//     public int add(int num1, int num2) {
+//         int sum = num1 + num2;
+//         return sum;
+//     }
+// }
+
+// class Store {
+//     public void music() {
+//         System.out.println("Play the Music");
+//     }
+
+//     public String buyPen(int price) {
+//         if (price >= 5)
+//             return "You Can Buy a Pen";
+//         return "You Can't Buy a Pen";
+//     }
+// }
+
+// class overLoading {
+//     public int add(int num1, int num2) {
+//         return num1 + num2;
+//     }
+
+//     public int add(int num1, int num2, int num3) {
+//         return num1 + num2 + num3;
+//     }
+
+//     public double add(double num1, int num2) {
+//         return num1 + num2;
+//     }
+// }
+
+public class Demo4 {
+    public static void main(String[] args) {
+        // 13. Class & Object Theory In Java
+        // Class
+        // int a = 5;
+        // int b = 5;
+
+        // Math calc = new Math();
+
+        // int result = calc.add(a, b);
+
+        // System.out.println(result);
+
+        // 14. Methods In Java
+        // Store obj = new Store();
+        // obj.music();
+
+        // String str = obj.buyPen(5);
+        // System.out.println(str);
+
+        // 15. Method Overloading In Java
+        // overLoading obj = new overLoading();
+        // int r1 = obj.add(5, 5);
+        // int r2 = obj.add(5, 7, 2);
+        // int r3 = obj.add(5, 8);
+
+        // System.out.println(r1);
+        // System.out.println(r2);
+        // System.out.println(r3);
+    }
+}
