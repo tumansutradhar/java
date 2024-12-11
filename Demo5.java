459b695aadfd297dd24bb72f4342f8748885406b
tumansutradhar <officialbishalsutradhar@gmail.com>
2024-12-11 19:12:11 +0530

    add array

---
+++ Demo5.java
@@ -0,0 +1,87 @@
+public class Demo5 {
+    public static void main(String[] args) {
+        // 17. Array In Java
+        // int newArray[] = { 55, 39, 43, 78 };
+
+        // System.out.println(newArray[2]);
+
+        // newArray[2] = 99; // Replace The Value Of Index Number 2
+
+        // System.out.println(newArray[2]);
+
+        // int num[] = new int[5];
+
+        // num[0] = 4;
+        // num[1] = 46;
+        // num[2] = 67;
+        // num[3] = 567;
+        // num[4] = 23;
+
+        // System.out.println(num[4]);
+
+        // for (int i = 0; i <= 3; i++) { // Print All The Values Of Array
+        // System.out.println(num[i]);
+        // }
+
+        // 18. Multi Dimensional Array In Java
+        // int mulArray[][] = new int[2][4];
+
+        // for (int i = 0; i < 2; i++) {
+        // for (int j = 0; j <= 3; j++) {
+        // mulArray[i][j] = (int) (Math.random() * 10);
+        // }
+        // }
+
+        // for (int i = 0; i < 2; i++) {
+        // for (int j = 0; j <= 3; j++) {
+        // System.out.print(mulArray[i][j] + " ");
+        // }
+        // System.out.println();
+        // }
+
+        // for (int n[] : mulArray) {
+        // for (int m : n) {
+        // System.out.print(m + " ");
+        // }
+        // System.out.println();
+        // }
+
+        // 19. Jagged And 3D Array In Java
+        // int array[][] = new int[2][]; // Jagged
+
+        // array[0] = new int[4];
+        // array[1] = new int[3];
+
+        // for (int i = 0; i < array.length; i++) {
+        // for (int j = 0; j < array[i].length; j++) {
+        // array[i][j] = (int) (Math.random() * 10);
+        // }
+        // }
+
+        // for (int i = 0; i < array.length; i++) {
+        // for (int j = 0; j < array[i].length; j++) {
+        // System.out.print(array[i][j] + " ");
+        // }
+        // System.out.println();
+        // }
+
+        // int array[][][] = new int[2][5][5]; // 3D Array
+
+        // for (int i = 0; i < array.length; i++) {
+        // for (int j = 0; j < array[i].length; j++) {
+        // for (int k = 0; k < array[i][j].length; k++) {
+        // array[i][j][k] = (int) (Math.random() * 10);
+        // }
+        // }
+        // }
+
+        // for (int i = 0; i < array.length; i++) {
+        // for (int j = 0; j < array[i].length; j++) {
+        // for (int k = 0; k < array[i][j].length; k++) {
+        // System.out.print(array[i][j][k] + " ");
+        // }
+        // }
+        // System.out.println();
+        // }
+    }
+}
