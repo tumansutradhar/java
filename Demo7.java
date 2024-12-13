bf3e1dbfff6a7fb9671f54c3bac6ed770f2dbc8e
tumansutradhar <officialbishalsutradhar@gmail.com>
2024-12-13 19:26:11 +0530

    add static variable

---
+++ Demo7.java
@@ -0,0 +1,40 @@
+
+// class staticVar {
+//     String brand;
+//     int price;
+//     static String type;
+
+//     public void show() {
+//         System.out.println("The brand is " + brand + " & The price is " + price + " & The type is " + type);
+//     }
+// }
+
+public class Demo7 {
+    public static void main(String[] args) {
+        // 23. StringBuffer & StringBuilder In Java
+        // StringBuffer sb = new StringBuffer();
+        // StringBuffer sb1 = new StringBuffer("Tuman");
+
+        // sb1.append(" Sutradhar");
+
+        // System.out.println(sb.capacity());
+        // System.out.println(sb1.capacity());
+        // System.out.println(sb1);
+
+        // 24. Static Variable In Java
+        // staticVar obj1 = new staticVar();
+        // obj1.brand = "Samsung";
+        // obj1.price = 17500;
+        // staticVar.type = "Smart Phone";
+
+        // staticVar obj2 = new staticVar();
+        // obj2.brand = "Xiaomi";
+        // obj2.price = 14749;
+        // staticVar.type = "Smart Phone";
+
+        // staticVar.type = "Cell Phone";
+
+        // obj1.show();
+        // obj2.show();
+    }
+}
