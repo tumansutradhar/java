ad708d79342ff37b9bb3ec186a26c7cf1366dbec
tumansutradhar <officialbishalsutradhar@gmail.com>
2024-12-13 23:07:20 +0530

    add static method and block

--- Demo7.java
+++ Demo7.java
@@ -1,5 +1,14 @@
-
 // class staticVar {
+//     String brand;
+//     int price;
+//     static String type; //Static Variable Belongs To The Class Not To The Object
+
+//     public void show() {
+//         System.out.println("The brand is " + brand + " & The price is " + price + " & The type is " + type);
+//     }
+// }
+
+// class staticMethod {
 //     String brand;
 //     int price;
 //     static String type;
@@ -7,10 +16,35 @@
 //     public void show() {
 //         System.out.println("The brand is " + brand + " & The price is " + price + " & The type is " + type);
 //     }
+
+//     public static void test(staticMethod a) {
+//         System.out.println("The brand is " + a.brand + " & The price is " + a.price + " & The type is " + type);
+//     }
+// }
+
+// class staticBlock {
+//     String brand;
+//     int price;
+//     static String type;
+
+//     public staticBlock() {
+//         brand = "NULL";
+//         price = 0;
+//         System.out.println("In Constructor");
+//     }
+
+//     static {
+//         type = "NONE";
+//         System.out.println("In Static Block");
+//     }
+
+//     public void show() {
+//         System.out.println("The brand is " + brand + " & The price is " + price + " & The type is " + type);
+//     }
 // }
 
 public class Demo7 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
         // 23. StringBuffer & StringBuilder In Java
         // StringBuffer sb = new StringBuffer();
         // StringBuffer sb1 = new StringBuffer("Tuman");
@@ -32,9 +66,37 @@ public class Demo7 {
         // obj2.price = 14749;
         // staticVar.type = "Smart Phone";
 
-        // staticVar.type = "Cell Phone";
+        // staticVar.type = "Cell Phone"; // This Affects Both obj1 & obj2
 
         // obj1.show();
         // obj2.show();
+
+        // 25. Static Method In Java
+        // staticMethod obj1 = new staticMethod();
+        // obj1.brand = "Samsung";
+        // obj1.price = 17500;
+        // staticMethod.type = "Smart Phone";
+
+        // staticMethod obj2 = new staticMethod();
+        // obj2.brand = "Xiaomi";
+        // obj2.price = 14749;
+        // staticMethod.type = "Smart Phone";
+
+        // staticMethod.type = "Cell Phone";
+
+        // obj1.show();
+        // obj2.show();
+
+        // staticMethod.test(obj1);
+
+        // 26. Static Block In Java
+        // staticBlock obj1 = new staticBlock();
+        // obj1.brand = "Samsung";
+        // obj1.price = 17500;
+        // staticBlock.type = "Smart Phone";
+
+        // staticBlock obj2 = new staticBlock();
+
+        // Class.forName("staticBlock");
     }
 }
