2736b12d1d5324878a4aad97c8c4908fea51b3f6
tumansutradhar <officialbishalsutradhar@gmail.com>
2024-12-16 18:57:26 +0530

    add getter and setter

---
+++ Demo8.java
@@ -0,0 +1,78 @@
+// class Student {
+//     private String name = "Tuman";
+//     private int roll = 20223045;
+
+//     public String testName() {
+//     return name;
+//     }
+
+//     public int testRoll() {
+//     return roll;
+//     }
+
+//     private String name;
+//     private int roll;
+
+//     public String testName() {
+//         return name;
+//     }
+
+//     public void setName(String s) {
+//         name = s;
+//     }
+
+//     public int testRoll() {
+//         return roll;
+//     }
+
+//     public void setRoll(int i) {
+//         roll = i;
+//     }
+
+// }
+
+// class newStudent {
+//     private String name;
+//     private int roll;
+
+//     public String getName() {
+//         return name;
+//     }
+
+//     public void setName(String name) {
+//         this.name = name;
+//     }
+
+//     public int getRoll() {
+//         return roll;
+//     }
+
+//     public void setRoll(int roll) {
+//         this.roll = roll;
+//     }
+
+// }
+
+public class Demo8 {
+    public static void main(String[] args) {
+        // 27. Encapsulation In Java
+        // Student a = new Student();
+        // a.name = "Tuman";
+        // a.roll = 20223045;
+
+        // System.out.println(a.testName() + " & " + a.testRoll());
+
+        // Student a = new Student();
+        // a.setName("Tuman Sutradhar");
+        // a.setRoll(20223045);
+
+        // System.out.println(a.testName() + " & " + a.testRoll());
+
+        // 28. Getters & Setters In Java
+        // newStudent s = new newStudent();
+        // s.setName("Bishal");
+        // s.setRoll(45);
+
+        // System.out.println(s.getName() + " & " + s.getRoll());
+    }
+}
