1faf1327ddff28515112661a37ec5f481210bc61
tumansutradhar <officialbishalsutradhar@gmail.com>
2024-12-18 23:41:17 +0530

    add constructor

---
+++ Demo9.java
@@ -0,0 +1,70 @@
+// class Constructor {
+//     String name;
+//     int roll;
+
+//     public Constructor() { // Constructor Name Should Be Same As Class Name
+//         System.out.println("This is Constructor");
+//     }
+
+//     public constructor() {
+//         name = "Nothing";
+//         roll = 0;
+//     }
+// }
+
+// class newConstructor {
+//     String name;
+//     int roll;
+
+//     public newConstructor() { // Default Constructor
+//         name = "Nothing";
+//         roll = 0;
+//     }
+
+//     public newConstructor(String name, int roll) { // Parameterized Constructor
+//         this.name = name;
+//         this.roll = roll;
+//     }
+// }
+
+// class Anonymous {
+//     public Anonymous() {
+//         System.out.println("Object Created");
+//     }
+
+//     public void show() {
+//         System.out.println("SHOW");
+//     }
+// }
+
+public class Demo9 {
+    public static void main(String[] args) {
+        // 30. Constructor In Java
+        // Constructor std = new Constructor();
+
+        // System.out.println("The Name is: " + std.name + " & The Roll is: " +
+        // std.roll);
+
+        // std.name = "Tuman";
+        // std.roll = 20223045;
+
+        // System.out.println("The Name is: " + std.name + " & The Roll is: " +
+        // std.roll);
+
+        // 31. Default VS Parameterized Constructor In Java
+        // newConstructor newCtr = new newConstructor(); // Default Constructor
+
+        // System.out.println("The Name is: " + newCtr.name + " & The Roll is: " +
+        // newCtr.roll);
+
+        // newConstructor newCtr1 = new newConstructor("Tuman", 20223045); //
+        // Parameterized Constructor
+
+        // System.out.println("The Name is: " + newCtr1.name + " & The Roll is: " +
+        // newCtr1.roll);
+
+        // 32. Anonymous Object In Java
+        // new Anonymous(); // This is Anonymous Object
+        // new Anonymous().show();
+    }
+}
