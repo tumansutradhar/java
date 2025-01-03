003b2020bd517f63331e539f02a10791758e19e6
tumansutradhar <officialbishalsutradhar@gmail.com>
2025-01-03 11:40:36 +0530

    add method overriding

---
+++ AdvancedCalculator.java
@@ -0,0 +1,6 @@
+public class AdvancedCalculator extends BasicCalculator {
+    public double power(int n1, int n2) {
+        return Math.pow(n1, n2);
+    }
+
+}
--- Demo10.java
+++ Demo10.java
@@ -1,3 +1,55 @@
+// class A {
+//     public A() {
+//         super(); // Every Constructor Has A super() Call, Even If You Don't Mention It Explicitly
+//         System.out.println("In A");
+//     }
+
+//     public A(int i) {
+//         super();
+//         System.out.println("Parametrize Constructor A");
+//     }
+// }
+
+// class B extends A {
+//     public B() {
+//         super();
+//         System.out.println("In B");
+//     }
+
+//     public B(int i) {
+//         this();
+//         System.out.println("Parametrize Constructor B");
+//     }
+// }
+
+// class A {
+//     public void show() {
+//         System.out.println("Method Show In A");
+//     }
+
+//     public void config() {
+//         System.out.println("Method Config In A");
+//     }
+// }
+
+// class B extends A {
+//     public void show() {
+//         System.out.println("Method Show In B");
+//     }
+// }
+
+// class MyCalculator {
+//     public int add(int n1, int n2) {
+//         return n1 + n2;
+//     }
+// }
+
+// class CalculatorOverriding extends MyCalculator {
+//     public int add(int n1, int n2) {
+//         return n1 + n2 + 1;
+//     }
+// }
+
 public class Demo10 {
     public static void main(String[] args) {
         // 34. Inheritance In Java
@@ -22,6 +74,26 @@ public class Demo10 {
         // System.out.println(division);
 
         // 35. Single & Multilevel Inheritance In Java
+        // AdvancedCalculator obj = new AdvancedCalculator();
 
+        // double power = obj.power(5, 5);
+
+        // System.out.println(power);
+
+        // 36. This & Super Method In Java
+        // B obj = new B();
+        // B Obj = new B(5);
+
+        // 37. Method Overriding In Java
+        // B obj = new B();
+
+        // obj.show();
+        // obj.config();
+
+        // CalculatorOverriding calculator = new CalculatorOverriding();
+
+        // int result = calculator.add(5, 4);
+
+        // System.out.println(result);
     }
 }
