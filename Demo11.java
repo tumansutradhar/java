fe7c4becb775b08226875078e9073811d6fc6823
tumansutradhar <officialbishalsutradhar@gmail.com>
2025-01-09 16:34:20 +0530

    add final keyword

--- Demo11.java
+++ Demo11.java
@@ -7,6 +7,50 @@
 //     }
 // }
 
+// class Dynamic {
+//     public void show() {
+//         System.out.println("In Dynamic Class");
+//     }
+// }
+
+// class DynamicMethodDispatch extends Dynamic {
+//     public void show() {
+//         System.out.println("In Dynamic Method Dispatch Class");
+//     }
+// }
+
+// final class Calculator {
+//     public void show() {
+//         System.out.print("Result: ");
+//     }
+
+//     public void add(int n1, int n2) {
+//         System.out.println(n1 + n2);
+//     }
+// }
+
+// class AdvanceCalculator extends Calculator {
+//     public void subtraction(int n1, int n2) {
+//         System.out.println(n1 - n2);
+//     }
+// }
+
+// class Calculator {
+//     public final void show() {
+//         System.out.print("Result: ");
+//     }
+
+//     public void add(int n1, int n2) {
+//         System.out.println(n1 + n2);
+//     }
+// }
+
+// class AdvanceCalculator extends Calculator {
+//     public void show() {
+//         System.out.println("Answer: ");
+//     }
+// }
+
 public class Demo11 {
     public static void main(String[] args) {
         // 38. Packages In Java
@@ -23,5 +67,32 @@ public class Demo11 {
 
         // Modifiers2 obj2 = new Modifiers2();
 
+        // 40. Polymorphism In Java [Poly Means Many & Morphism Means Behavior]
+        // 1. Compile-time (or Static) Polymorphism: Achieved using method overloading
+        // and operator overloading.
+        // 2. Runtime (or Dynamic) Polymorphism: Achieved using method overriding.
+
+        // 41. Dynamic Method Dispatch In Java
+        // DynamicMethodDispatch dmp = new DynamicMethodDispatch();
+        // dmp.show();
+
+        // Dynamic d = new DynamicMethodDispatch(); // "d" Type Is "dynamic" & Its Implementation Is From The Class "DynamicMethodDispatch"
+        // d.show();
+
+        // 42. Final Keyword In Java [Final Means Constant]
+        // Final With -> Variable, Class, Method
+        // 1. Variable
+        // final int n = 99;
+        // n = 100;
+        // System.out.println(n);
+
+        // 2. Class
+        // Calculator cal = new Calculator();
+        // cal.show();
+        // cal.add(5, 5);
+
+        // 3. Method
+        // AdvanceCalculator advCal = new AdvanceCalculator();
+        // advCal.show();
     }
 }
