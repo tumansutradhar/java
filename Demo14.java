dbb12ba8a5f7dd06597f0b7a0d1d6ff2856a28e4
tumansutradhar <officialbishalsutradhar@gmail.com>
2025-01-13 22:54:03 +0530

    need of interface

---
+++ Demo14.java
@@ -0,0 +1,76 @@
+// class to class: extends
+// class to interface: implements
+// interface to interface: extends
+
+// interface i {
+//     void display();
+// }
+
+// interface n {
+//     void show();
+// }
+
+// interface t {
+//     void info();
+// }
+
+// interface e extends i, n {
+// }
+
+// class c implements t, e {
+//     public void display() {
+//         System.out.println("Displaying content...");
+//     }
+
+//     public void show() {
+//         System.out.println("Showing details...");
+//     }
+
+//     public void info() {
+//         System.out.println("Providing information...");
+//     }
+// }
+
+// class Computer {
+//     public void code() {
+//     }
+// }
+
+// class Desktop extends Computer {
+//     public void code() {
+//         System.out.println("Work on projects using a Desktop");
+//     }
+// }
+
+// class Laptop extends Computer {
+//     public void code() {
+//         System.out.println("Work on projects using a Laptop");
+//     }
+// }
+
+// class Developer {
+//     public void develop(Computer c) {
+//         c.code();
+//     }
+// }
+
+public class Demo14 {
+    public static void main(String[] args) {
+        // 51. More On Interfaces In Java
+        // c obj = new c();
+        // obj.display();
+        // obj.show();
+        // obj.info();
+
+        // 52. Need Of Interface In Java
+        // Computer desktop = new Desktop();
+        // Computer laptop = new Laptop();
+
+        // Developer dev = new Developer();
+        // dev.develop(desktop);
+        // dev.develop(laptop);
+
+        // 53. Enumeration(Enum) In Java
+        
+    }
+}
