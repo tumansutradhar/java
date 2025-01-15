dc29e3624b80a4a7180f90d3de339535afc03b52
tumansutradhar <officialbishalsutradhar@gmail.com>
2025-01-15 11:09:39 +0530

    add enum class

---
+++ Demo15.java
@@ -0,0 +1,37 @@
+// enum Laptop {
+//     Macbook(69000), Zenbook(56000), TufGaming(60000);
+
+//     private int price;
+
+//     private Laptop(int price) {
+//         this.price = price;
+//     }
+
+//     public int getPrice() {
+//         return price;
+//     }
+
+//     public void setPrice(int price) {
+//         this.price = price;
+//     }
+// }
+
+public class Demo15 {
+    public static void main(String[] args) {
+        // 55. Enum Class In Java
+        // Laptop laptop = Laptop.Macbook;
+
+        // System.out.println("Price of " + laptop + ": " + laptop.getPrice());
+
+        // laptop.setPrice(45000);
+
+        // System.out.println("Updated price of " + laptop + ": " + laptop.getPrice());
+
+        // for (Laptop laptop : Laptop.values()) {
+        // System.out.println("Price of " + laptop + ": " + laptop.getPrice());
+        // }
+
+        // 56. Annotation In Java
+
+    }
+}
