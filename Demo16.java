6f8ff8fba0aed28b2f9e11d9a583f7a46b499b3c
tumansutradhar <officialbishalsutradhar@gmail.com>
2025-01-23 10:00:26 +0530

    add multiple catch blocks

---
+++ Demo16.java
@@ -0,0 +1,47 @@
+// @FunctionalInterface
+// interface lambdaReturn {
+//     int add(int n1, int n2);
+// }
+
+public class Demo16 {
+    public static void main(String[] args) {
+        // 59. Lambda Expression With Return
+        // lambdaReturn obj = (a, b) -> a + b;
+
+        // int result = obj.add(5, 5);
+        // System.out.println(result);
+
+        // 60. Types Of Interface In Java
+        // a) Marker Interface[doesn't have any methods or fields]
+        // b) Single Abstract Method (SAM) Interface / Functional Interface[exactly one abstract method]
+        // c) Normal Interface[multiple abstract methods as well as default and static methods]
+
+        // 61. Exception Handling Using "try-catch" In Java
+        // int n1 = 2;
+        // int n2 = 0;
+
+        // try {
+        // n2 = 18 / n1;
+        // } catch (Exception e) {
+        // System.out.println("Something Went Wrong!");
+        // }
+
+        // System.out.println(n2);
+
+        // 62. Multiple Catch Blocks In Java
+        // try {
+        // int result = 10 / 0;
+        // int num[] = new int[5];
+        // num[11] = 50;
+        // String str = null;
+
+        // System.out.println(str.length());
+        // } catch (ArithmeticException e) {
+        // System.out.println("Cannot divide by zero!");
+        // } catch (ArrayIndexOutOfBoundsException e) {
+        // System.out.println("Array Index Out Of Bounds Exception");
+        // } catch (Exception e) {
+        // System.out.println(e.getMessage());
+        // }
+    }
+}
