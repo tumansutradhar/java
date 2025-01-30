1061382a599438c88a117356a80123b61a28a536
tumansutradhar <officialbishalsutradhar@gmail.com>
2025-01-30 18:32:06 +0530

    add foreach method

---
+++ Demo21.java
@@ -0,0 +1,58 @@
+// import java.util.Arrays;
+// import java.util.List;
+// import java.util.function.Consumer;
+
+public class Demo21 {
+    public static void main(String[] args) {
+        // 77. Stream API In Java
+        // List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+
+        // Stream<Integer> stream = numbers.stream();
+        // Stream<Integer> evenStream = stream.filter(i -> i % 2 == 0);
+
+        // evenStream.forEach(i -> System.out.println(i));
+
+        // int s = numbers.stream().filter(i -> i % 2 == 0).map(i -> i * 2).reduce(0, Integer::sum);
+
+        // System.out.println(s);
+
+        // for (int i : numbers) {
+        // if (i % 2 == 0) {
+        // System.out.println(i);
+        // }
+        // }
+
+        // for (int i = 0; i < numbers.size(); i++) {
+        // System.out.println(numbers.get(i));
+        // }
+
+        // for (int i : numbers) {
+        // System.out.println(i);
+        // }
+
+        // numbers.forEach(i -> System.out.println(i));
+
+        // =============================================
+
+        // =============================================
+
+        // 78. "forEach" Method In Java
+        // List<String> names = Arrays.asList("Alice", "Bob", "Charlie");
+
+        // Consumer<String> consumer = new Consumer<String>() {
+        // public void accept(String i) {
+        // System.out.println(i);
+        // }
+        // };
+
+        // OR
+
+        // Consumer<String> consumer = (i) -> System.out.println(i);
+
+        // names.forEach(consumer);
+
+        // OR
+
+        // names.forEach(name -> System.out.println(name));
+    }
+}
